52319cce3c267d97836609f57ed3e662d261a5ac
DAVI CORRADI <159476924+Davi-Corradi@users.noreply.github.com>
2025-09-19 08:56:03 -0300

    Add files via upload

---
+++ CC4MB/POO 2/WpfApp1/BD/BD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.model;
+
+namespace WpfApp1.BD
+{
+    internal class BD
+    {
+        public static List<IdeiaInovacao> mybd = new();
+        
+        public static  void SalvarBD(IdeiaInovacao i) => mybd.Add(i);
+
+        public static List<IdeiaInovacao> RetornarBD() => mybd;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ CC4MB/POO 2/WpfApp1/model/IdeiaInovacao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.model
+{
+    internal class IdeiaInovacao
+    {
+        public string Area { get; set; }
+        public string Ideia { get; set; } = string.Empty;
+        public float Custo { get; set; }
+
+        public Boolean CadastrarII(IdeiaInovacao i)
+        {
+            BD.BD.SalvarBD(i);
+            
+            return true;
+        }
+
+        public override string ToString()
+        {
+                return $"{Area} / {Ideia} / {Custo}";
+              // return base.ToString()
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
