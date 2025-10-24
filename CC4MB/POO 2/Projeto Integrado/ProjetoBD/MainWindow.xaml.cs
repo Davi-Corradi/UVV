fd4765c93c7ec08c1fda0d6705fb16e3eb3090c4
DAVI CORRADI <159476924+Davi-Corradi@users.noreply.github.com>
2025-10-24 08:48:38 -0300

    Add files via upload

---
+++ CC4MB/POO 2/Projeto Integrado/ProjetoBD/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetoBD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_4(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_salvar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_pesquisar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_excluir(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_limpar(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
