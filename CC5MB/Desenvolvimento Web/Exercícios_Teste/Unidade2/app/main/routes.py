d6da8966d0bf21a6a23d0988c05d07e9703d89fa
Davi Corradi <159476924+Davi-Corradi@users.noreply.github.com>
2026-02-23 10:41:11 -0300

    Add files via upload

---
+++ CC5MB/Desenvolvimento Web/Exercícios_Teste/Unidade2/app/main/routes.py
@@ -0,0 +1,9 @@
+from flask import current_app
+from app.main import main
+
+@main.route('/')
+@main.route('/index')
+
+def index():
+    ambiente = current_app.config.get('CONFIG_NAME', "desconhecido")
+    return f'Ola, mundo! Ambiente atual: {ambiente}'  
